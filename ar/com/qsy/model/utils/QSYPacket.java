d5aa67aa96873796301ca9897ad658b27313fd6c
Ramiro <ramiroromero94@gmail.com>
2017-06-13 23:11:31 -0300

    Usar enums. falta generar el arreglo de bytes

--- ar/com/qsy/model/utils/QSYPacket.java
+++ ar/com/qsy/model/utils/QSYPacket.java
@@ -2,6 +2,7 @@ package ar.com.qsy.model.utils;
 
 import java.awt.Color;
 import java.net.InetAddress;
+import java.util.Arrays;
 
 public final class QSYPacket {
 
@@ -46,39 +47,61 @@ public final class QSYPacket {
 	public static final byte TYPE_HELLO = 0x00;
 	public static final byte TYPE_COMMAND = 0x01;
 	public static final byte TYPE_TOUCHE = 0x02;
-	public static final byte TYPE_KEEP_ALIVE = 0x03;
+	public static final byte TYPE_KEEPALIVE = 0x03;
 
 	private static final int RED_COLOR = 0xF000;
 	private static final int GREEN_COLOR = 0x0F00;
 	private static final int BLUE_COLOR = 0x00F0;
 	private static final int WHITE_COLOR = 0x000F;
 	private static final int NO_COLOR = 0x0000;
+	
+	public enum PacketType {
+		Hello, Command, Touche, Keepalive
+	}
 
 	private final InetAddress nodeAddress;
-	private final short type;
 	private final int id;
 	private final Color color;
 	private final long delay;
+	private final PacketType packetType;
+	private final byte[] rawData;
 
-	private QSYPacket(final InetAddress nodeAddress, final short type, final int id, final Color color, final long delay) {
+	private QSYPacket(final InetAddress nodeAddress, final PacketType type, final int id, final Color color, final long delay) {
 		this.nodeAddress = nodeAddress;
-		this.type = type;
 		this.id = id;
 		this.color = color;
 		this.delay = delay;
+		this.packetType = type;
+		// TODO: generar el arreglo de bytes a partir de estos datos
 	}
 
 	public QSYPacket(final InetAddress nodeAddress, final byte[] data) throws IllegalArgumentException {
+		rawData = Arrays.copyOf(data, data.length);
+		
 		if (data.length != PACKET_SIZE) {
-			throw new IllegalArgumentException("<< QSY_PACKET_ERROR >> El tamaño del QSYPacket debe ser de " + PACKET_SIZE + " .");
+			throw new IllegalArgumentException("<< QSY_PACKET_ERROR >> El tamaño del QSYPacket debe ser de " + PACKET_SIZE + ".");
 		} else if (data[Q_INDEX] != 'Q' || data[S_INDEX] != 'S' || data[Y_INDEX] != 'Y') {
-			throw new IllegalArgumentException("<< QSY_PACKET_ERROR >> El QSYPacket posee una firma invalida.");
+			throw new IllegalArgumentException("<< QSY_PACKET_ERROR >> El QSYPacket posee una firma inválida.");
 		}
+		
 		this.nodeAddress = nodeAddress;
-		this.type = (short) getUnsignedValue(data, TYPE_INDEX, TYPE_INDEX);
-		if (type != TYPE_HELLO && type != TYPE_COMMAND && type != TYPE_KEEP_ALIVE && type != TYPE_TOUCHE) {
+		switch ((short) getUnsignedValue(data, TYPE_INDEX, TYPE_INDEX)) {
+		case TYPE_HELLO:
+			this.packetType = PacketType.Hello;
+			break;
+		case TYPE_COMMAND:
+			this.packetType = PacketType.Command;
+			break;
+		case TYPE_TOUCHE:
+			this.packetType = PacketType.Touche;
+			break;
+		case TYPE_KEEPALIVE:
+			this.packetType = PacketType.Keepalive;
+			break;
+		default:
 			throw new IllegalArgumentException("<< QSY_PACKET_ERROR >> El QSYPacket posee un type invalido.");
 		}
+
 		this.id = (int) getUnsignedValue(data, ID_INDEX, (byte) (ID_INDEX + 1));
 		this.color = getColor((int) getUnsignedValue(data, COLOR_RG_INDEX, COLOR_BW_INDEX));
 		this.delay = (long) getUnsignedValue(data, DELAY_INDEX, (byte) (DELAY_INDEX + 3));
@@ -118,8 +141,8 @@ public final class QSYPacket {
 		return nodeAddress;
 	}
 
-	public short getType() {
-		return type;
+	public PacketType getType() {
+		return packetType;
 	}
 
 	public int getId() {
@@ -154,20 +177,20 @@ public final class QSYPacket {
 	public String toString() {
 		final StringBuilder stringBuilder = new StringBuilder();
 		switch (getType()) {
-		case TYPE_HELLO: {
+		case Hello: {
 			stringBuilder.append("QSYHelloPacket");
 			break;
 		}
-		case TYPE_COMMAND: {
+		case Command: {
 			stringBuilder.append("QSYCommandPacket");
 			break;
 		}
-		case TYPE_TOUCHE: {
+		case Touche: {
 			stringBuilder.append("QSYTouchePacket");
 			break;
 		}
-		case TYPE_KEEP_ALIVE: {
-			stringBuilder.append("QSYKeepAlivePacket");
+		case Keepalive: {
+			stringBuilder.append("QSYKeepalivePacket");
 			break;
 		}
 		}
@@ -202,7 +225,7 @@ public final class QSYPacket {
 		} else if (delay < MIN_DELAY_SIZE || delay > MAX_DELAY_SIZE) {
 			throw new IllegalArgumentException("<< QSY_PACKET_ERROR >> El delay debe estar entre [" + MIN_DELAY_SIZE + " ; " + MAX_DELAY_SIZE + "]");
 		} else {
-			return new QSYPacket(nodeAddress, TYPE_COMMAND, nodeId, color, delay);
+			return new QSYPacket(nodeAddress, PacketType.Command, nodeId, color, delay);
 		}
 	}
 
