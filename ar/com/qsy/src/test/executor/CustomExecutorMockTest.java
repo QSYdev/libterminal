21b61bf6bc676056272a52da7b85afa4efbf3149
Matias Pan <matias.pan26@gmail.com>
2017-07-23 21:10:53 -0300

    array de steps en test

--- ar/com/qsy/src/test/executor/CustomExecutorMockTest.java
+++ ar/com/qsy/src/test/executor/CustomExecutorMockTest.java
@@ -20,10 +20,7 @@ public class CustomExecutorMockTest {
 	public int NUMBER_OF_STEPS;
 	public Routine routine;
 	public TreeMap<Integer, Integer> nodesIdsAssociations;
-	public boolean[] stepsStopOnTimeouts;
-	public long[] stepsTimeouts;
-	public String[] stepsExpressions;
-	public ArrayList<LinkedList<NodeConfiguration>> stepsNodesConfigurations;
+	public ArrayList<Step> steps;
 
 	@BeforeEach
 	public void setUp() {
@@ -32,19 +29,8 @@ public class CustomExecutorMockTest {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		NUMBER_OF_STEPS = routine.getSteps().size();
-		stepsStopOnTimeouts = new boolean[NUMBER_OF_STEPS];
-		stepsExpressions = new String[NUMBER_OF_STEPS];
-		stepsTimeouts = new long[NUMBER_OF_STEPS];
-		stepsNodesConfigurations = new ArrayList<>();
-		Iterator rt = routine.iterator();
-		for(int i=0; i < NUMBER_OF_STEPS; i++) {
-			Step step = (Step) rt.next();
-			stepsExpressions[i] = step.getExpression();
-			stepsStopOnTimeouts[i] = step.getStopOnTimeout();
-			stepsTimeouts[i] = step.getTimeOut();
-			stepsNodesConfigurations.add(step.getNodesConfiguration());
-		}
+		steps = routine.getSteps();
+		NUMBER_OF_STEPS = steps.size();
 		nodesIdsAssociations = new TreeMap<>(); nodesIdsAssociations.put(1, 0); nodesIdsAssociations.put(2, 3);
 		executor = new CustomExecutorMock(routine, nodesIdsAssociations);
 		try {
@@ -58,12 +44,12 @@ public class CustomExecutorMockTest {
 	public void getNextStep() {
 		for(int i = 1; i < NUMBER_OF_STEPS; i++) {
 			Step step = executor.getNextStep();
-			assertEquals(stepsExpressions[i], step.getExpression(), "Expresion de paso incorrecta");
-			assertEquals(stepsStopOnTimeouts[i], step.getStopOnTimeout(), "Condicion de timeout incorrecta");
-			assertEquals(stepsTimeouts[i], step.getTimeOut(), "Timeout de paso incorrecto");
-			assertEquals(stepsNodesConfigurations.get(i), step.getNodesConfiguration(), "Configuracion de nodos incorrecta");
+			assertEquals(steps.get(i).getExpression(), step.getExpression(), "Expresion de paso incorrecta");
+			assertEquals(steps.get(i).getStopOnTimeout(), step.getStopOnTimeout(), "Condicion de timeout incorrecta");
+			assertEquals(steps.get(i).getTimeOut(), step.getTimeOut(), "Timeout de paso incorrecto");
+			assertEquals(steps.get(i).getNodesConfiguration(), step.getNodesConfiguration(), "Configuracion de nodos incorrecta");
 			for(NodeConfiguration nodeConfig : step.getNodesConfiguration()) {
-				assertEquals(true, nodesIdsAssociations.containsKey(nodeConfig.getId()), "Configuracion de paso incorrecta");
+				assertEquals(true, nodesIdsAssociations.containsKey(nodeConfig.getId()), "ID de nodo incorrecto");
 			}
 		}
 	}
