c395329c51e3a5f95bf3d35d02fd28aec50ca939
agusss.garcia19 <agusss.garcia19@gmail.com>
2017-10-11 22:59:53 -0300

    toString() de colores

--- src/main/java/libterminal/lib/routine/Color.java
+++ src/main/java/libterminal/lib/routine/Color.java
@@ -1,27 +1,51 @@
 package libterminal.lib.routine;
 
+import java.util.Hashtable;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public final class Color {
 
 	private static final byte MAX_AMOUNT_COLOR = 16;
 
-	private final byte red;
-	private final byte green;
-	private final byte blue;
-
 	public static final Color WHITE = new Color((byte) 0x0F, (byte) 0x0F, (byte) 0x0F);
 	public static final Color YELLOW = new Color((byte) 0x0F, (byte) 0x0F, (byte) 0x00);
-	public static final Color VIOLET = new Color((byte) 0x0F, (byte) 0x00, (byte) 0x0F);
+	public static final Color MAGENTA = new Color((byte) 0x0F, (byte) 0x00, (byte) 0x0F);
 	public static final Color RED = new Color((byte) 0x0F, (byte) 0x00, (byte) 0x00);
 	public static final Color CYAN = new Color((byte) 0x00, (byte) 0x0F, (byte) 0x0F);
 	public static final Color GREEN = new Color((byte) 0x00, (byte) 0x0F, (byte) 0x00);
 	public static final Color BLUE = new Color((byte) 0x00, (byte) 0x00, (byte) 0x0F);
 	public static final Color NO_COLOR = new Color((byte) 0x00, (byte) 0x00, (byte) 0x00);
 
+	private static final Hashtable<Color, String> hashTable;
+	static {
+		hashTable = new Hashtable<>();
+		hashTable.put(WHITE, "Blanco");
+		hashTable.put(YELLOW, "Amarillo");
+		hashTable.put(MAGENTA, "Magenta");
+		hashTable.put(RED, "Rojo");
+		hashTable.put(CYAN, "Cyan");
+		hashTable.put(GREEN, "Verde");
+		hashTable.put(BLUE, "Azul");
+		hashTable.put(NO_COLOR, "Sin Color");
+	}
+
+	private final byte red;
+	private final byte green;
+	private final byte blue;
+
+	private final int hashCode;
+
 	public Color(final byte red, final byte green, final byte blue) {
 		if (checkAmountOfColor(red) && checkAmountOfColor(green) && checkAmountOfColor(blue)) {
 			this.red = red;
 			this.green = green;
 			this.blue = blue;
+			final HashCodeBuilder hashBuilder = new HashCodeBuilder();
+			hashBuilder.append(red);
+			hashBuilder.append(green);
+			hashBuilder.append(blue);
+			this.hashCode = hashBuilder.toHashCode();
 		} else {
 			throw new IllegalArgumentException("<< COLOR >> La cantidad de color ingresada debe ser un valor entre 0 y " + MAX_AMOUNT_COLOR + "exclusive");
 		}
@@ -45,7 +69,16 @@ public final class Color {
 
 	@Override
 	public String toString() {
-		return "RED = " + red + " || GREEN = " + green + " || BLUE = " + blue;
+		final String result = hashTable.get(this);
+		if (result == null)
+			return "RED = " + red + " || GREEN = " + green + " || BLUE = " + blue;
+		else
+			return result;
+	}
+
+	@Override
+	public int hashCode() {
+		return hashCode;
 	}
 
 	@Override
